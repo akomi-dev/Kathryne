3c1a625ac43622d9ceb4547eb090e7deb169d3f4
Akomi <87880680+akomi-dev@users.noreply.github.com>
2022-05-30 01:13:59 -0400

    Commit

--- assets/commands/rank.js
+++ assets/commands/rank.js
@@ -2,10 +2,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('rewards')
-		.setDescription('Retrieve your guild rank rewards'),
+		.setName('rank')
+		.setDescription('Check your rank'),
 	async execute(interaction) {
-		const ranks = require("../ranks.json")
+		const ranks = require(require('path').resolve('assets', 'ranks.json'))
 		const userRank = ranks[interaction.member.user.id]["rank"]
 		await interaction.reply(`Your ranks is ${userRank}`);
 	},
--- assets/commands/wish.js
+++ assets/commands/wish.js
@@ -1,46 +1,71 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 const genshindb = require('genshin-db');
 const fs = require('node:fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('wish')
-		.setDescription('Wish for amazing rewards!')
-		.addIntegerOption(option =>
-			option.setName("quantity")
-			      .setDescription("Quantity of wishes wanted: Must be 1, 5, or 10")
-			      .setRequired(true)),
+		.setDescription('Wish for amazing rewards!'),
+
 	async execute(interaction) {
-		const wishAmt = interaction.options.get("quantity").value;
-		const cost = wishAmt*100;
+		const cost = 100;
 		
-		const ranksJSON = "Kathryne/assets/ranks.json";
+		const ranksJSON = require('path').resolve('assets', 'ranks.json');
 		let ranks = require(ranksJSON);
+		const authorId = interaction.user.id;
 		
 		const write = (file, obj) => fs.writeFile(
 			file, JSON.stringify(obj), err => { if (err) throw err; }
 		);
 		
-		if (wishAmt == 1 || wishAmt == 5 || wishAmt == 10) {
-			if (cost <= ranks[authorId]["coins"]) {
-				const authorId = interaction.user.id;
-				ranks[authorId]["coins"] -= cost;
-				for (let i=0; i <= wishAmt; i++) {
-					const weapons = genshindb.weapons('names', { matchCategories: true });
-					const randIndex = Math.floor(Math.random()*weapons.length);
-					ranks[authorId]["items"].push(weapons);
-					write(ranksJSON, ranks);
-
-					interaction.reply("Wish granted!")
-				}
-			} else {
-				await interaction.reply({ content: 'Insufficient Coins!', ephemeral: true });
-			}
-		} else {
-			await interaction.reply({
-				content: 'Quantity of wishes must be 1, 5, or 10', 
-				ephemeral: true
-			});
+		if (cost > ranks[authorId]["coins"]) {
+			return await interaction.reply({ content: 'Insufficient Coins!', ephemeral: true });
 		};
+
+		ranks[authorId]["coins"] -= cost;
+
+		const randNum = range => Math.floor(Math.random()*range);
+		let temp;
+		let item;
+		let image;
+		let rarity;
+		
+		switch (randNum(2)) {
+			case 0: 
+			{
+				const weapons = genshindb.weapons('names', { matchCategories: true });
+				const randIndexWeapons = randNum(weapons.length);
+				ranks[authorId]["items"].push(item = weapons[randIndexWeapons]);
+				temp = genshindb.weapons(item);
+				image = temp.images.image;
+				rarity = temp.rarity;
+			}
+			case 1: 
+			{
+				const characters = genshindb.characters('names', { matchCategories: true });
+				const randIndexCharacters = randNum(characters.length);
+				ranks[authorId]["items"].push(item = characters[randIndexCharacters]);
+				temp = genshindb.characters(item);
+				image = temp.images.icon;
+				rarity = temp.rarity;
+			}
+		};
+
+		write(ranksJSON, ranks);
+
+		const embed = new MessageEmbed()
+			.setColor('#3bc2ca')
+			.setTitle('Wish!')
+			.setURL('https://www.hoyolab.com/home')
+			.setAuthor({ 
+				name: `${interaction.user.username}#${interaction.user.discriminator}`,
+				iconURL: "https://github.com/akomi-dev/Kathryne/blob/main/assets/genshinLogoInverted.png?raw=true"
+			})
+			.setThumbnail(image)
+			.addFields({ name: item, value: `${rarity} ⭐` })
+			.setTimestamp(new Date())
+		
+		return await interaction.reply({ embeds: [embed] });
 	}
 };
